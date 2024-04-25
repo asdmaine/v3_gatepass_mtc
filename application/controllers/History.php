e0b708e0c1b182aa55b4fa92b37a1f8e42bdc2d4
Sulthon Sholahuddin <sulthon.sdn@gmail.com>
2024-04-25 09:06:32 +0700

    fix tulisan hilang di history dan all history

--- application/controllers/History.php
+++ application/controllers/History.php
@@ -11,6 +11,8 @@ class History extends CI_Controller
 			redirect('AuthAdmin/Login');
 		}else{
 			require_once 'set_menu.php';
+			$language = $this->session->userdata('language');
+			$this->lang->load('general', $language);
 		}
 	}
 	public function index()
---
+++ application/language/indonesian/db_lang.php
@@ -0,0 +1,50 @@
+<?php
+// $lang['e']='e';
+
+// DASHBOARD
+$lang['Akses ditolak']='Akses ditolak';
+$lang['Anda sudah gatepass hari ini']='Anda sudah gatepass hari ini';
+$lang['Gatepass anda Bulan ini'] = 'Gatepass anda Bulan ini';
+$lang['Gatepass anda Bulan lalu'] = 'Gatepass anda Bulan lalu';
+$lang['Gatepass anda Tahun ini'] = 'Gatepass anda Tahun ini';
+$lang['Total Gatepass Karyawan Bulan ini'] = 'Total Gatepass Karyawan Bulan ini';
+$lang['Total Gatepass Karyawan Bulan lalu'] = 'Total Gatepass Karyawan Bulan lalu';
+$lang['Total Gatepass Karyawan Tahun ini'] = 'Total Gatepass Karyawan Tahun ini';
+$lang['Progres Gatepass anda']='Progres Gatepass anda';
+$lang['Tanggal']='Tanggal';
+$lang['Keperluan']='Keperluan';
+$lang['Penjelasan']='Penjelasan';
+$lang['Aksi']='Aksi';
+$lang['Gatepass anda hari ini']='Gatepass anda hari ini';
+$lang['Set Signature']='Set Signature';
+$lang['Selesai']='Selesai';
+$lang['Bersihkan']='Bersihkan';
+$lang['Tekan Tombol Selesai terlebih dahulu']='Tekan Tombol Selesai terlebih dahulu';
+$lang['Apakah anda yakin?']='Apakah anda yakin?';
+$lang['Anda akan kehilangan progres dari gatepass anda']='Anda akan kehilangan progres dari gatepass anda';
+$lang['Kembali']='Kembali';
+$lang['Hapus']='Hapus';
+$lang['Perkiraan waktu']='Perkiraan waktu';
+$lang['Perkiraan jam keluar']='Perkiraan jam keluar';
+$lang['Perkiraan jam masuk']='Perkiraan jam masuk';
+
+// ALL_HISTORY
+$lang['Semua Histori Gatepass pada tahun']='Semua Histori Gatepass pada tahun ';
+
+// APPROVE
+$lang['Urusan Perusahaan']='Urusan Perusahaan';
+$lang['Urusan Keluarga']='Urusan Keluarga';
+$lang['Sakit/Klinik/Rs']='Sakit/Klinik/Rs';
+$lang['lain-lain']='lain-lain';
+$lang['Pengesahan']='Pengesahan';
+$lang['Konfirmasi']='Konfirmasi';
+
+// Send_mail
+$lang['DITERIMA DENGAN QRCODE']='DITERIMA DENGAN QRCODE ';
+
+//Submit
+$lang['Tidak boleh keluar lebih dari 3 jam']='Tidak boleh keluar lebih dari 3 jam ';
+$lang['dasbor']='Dasbor';
+$lang['keluar']='Keluar';
+
+
